a7d51d3e2dfef1889b322bf7ead1af6fbe5509d0
Christian101GTZ <christiangomez811@gmail.com>
2026-08-24 16:18:31 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -30,3 +30,35 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
  }
+
+const messageForm = document.querySelector("form[name='leave_message']");
+const messageSection = document.querySelector("#messages");
+const messageList = messageSection.querySelector("ul");
+
+messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+
+
+    console.log(name, email, message);
+
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `
+      <a href="mailto:${email}">${name}</a>
+      <span> - ${message}</span>
+    `;
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function() {
+      newMessage.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    event.target.reset();
+  });
